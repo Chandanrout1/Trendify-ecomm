22f66fbd53557f71fd2bc83fd8793fcf90ef26f7
Pravu chandan Rout <140379711+Chandanrout1@users.noreply.github.com>
2024-10-10 23:23:08 +0530

    add file implementation

---
+++ admin/src/pages/ListofFile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListofFile = () => {
+  return (
+    <div>ListofFile</div>
+  )
+}
+
+export default ListofFile
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,4 +1,4 @@
-import cloudinary from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import productModel from "../models/productModels.js";
 
 // add product
